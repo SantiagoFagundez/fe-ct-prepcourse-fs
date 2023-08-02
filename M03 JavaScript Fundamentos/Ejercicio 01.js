c10b3ec52b369966fbf2c58dc8fba7ad568fe32d
SantiagoFagundez <santiagoafagundez@gmail.com>
2023-08-01 22:21:01 -0300

    mi tercer commit

--- M03 JavaScript Fundamentos/Ejercicio 01.js
+++ M03 JavaScript Fundamentos/Ejercicio 01.js
@@ -5,7 +5,7 @@
 // por el correspondiente.
 
 // Crea una variable de tipo string.
-const nuevoString = Hola;
+const nuevoString = hola;
 
 // Crea una variable de tipo number.
 const nuevoNumero = 4;
@@ -17,10 +17,10 @@ const nuevoBoolean = true;
 const nuevaResta = 10 - 7 === 3;
 
 // Resuelve el siguiente problema matemático.
-const nuevaMultiplicacion = 10 * null === 40;
+const nuevaMultiplicacion = 10 * 10 === 40;
 
 // Resuelve el siguiente problema matemático.
-const nuevoModulo = 21 % 5 === null;
+const nuevoModulo = 21 % 5 === 1;
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
